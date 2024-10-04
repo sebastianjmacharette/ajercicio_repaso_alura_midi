d8c2d0d7eba0387692081a7b2494c2449953a4c0
sebastianjmacharette <dev@macharette.com.ar>
2024-10-04 15:37:19 -0300

    final

--- main.js
+++ main.js
@@ -7,26 +7,35 @@
 
 // document.querySelector('.tecla_pom').onclick = playSonidoPom;  // Asignas la función correctamente
 
- function playSonido(idElementoAudio){
-     const audio = document.querySelector(idElementoAudio);
-     audio.play();  // Aquí reproduces el sonido
+function playSonido(idElementoAudio) {
+  const audio = document.querySelector(idElementoAudio);
+  audio.play(); // Aquí reproduces el sonido
 }
 
-  //document.querySelector('.tecla_pom').onclick = playSonidoPom;  // Asignas la función correctamente
- const listaDeTeclas = document.querySelectorAll('.tecla');
-let contador = 0;
+//document.querySelector('.tecla_pom').onclick = playSonidoPom;  // Asignas la función correctamente
+const listaDeTeclas = document.querySelectorAll(".tecla");
 
-
- while (contador < 9) {
+for (let contador = 0; contador< listaDeTeclas.length; contador++) {
+  //inicializacion, la condicion y el invremento
   const tecla = listaDeTeclas[contador];
   const instrumento = tecla.classList[1]; //traemos el instrumento de la lista
+  console.log('numero de ID:' + contador)
   const idAudio = `#sonido_${instrumento}`; //string dinamico, can comillas invertidas
-  console.log(idAudio)
-  tecla.onclick = function (){
+  console.log(idAudio);
+  tecla.onclick = function () {
     playSonido(idAudio);
- 
   };
-  contador++;
+  tecla.onkeydown = function(evento){
 
-  
- }
+
+    if (evento.code === 'Space' || evento.code === 'Enter'){
+      tecla.classList.add('activa');
+    }
+    console.log(evento.code === 'Space' || evento.code === 'Enter')
+  }
+  tecla.onkeyup = function  (){
+    tecla.classList.remove('activa');
+  }
+  }
+
+ 
